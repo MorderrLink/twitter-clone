df68039a1f5824652788c7b54add7ec9f2f35d8f
MorderrLink <burmenskiy90@bk.ru>
2023-11-19 18:08:12 +0300

    Fixed SRC ProfileImage bug

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { VscAccount } from "react-icons/vsc";
 type ProfileImageProps = {
-    src?: string | null,
+    src: string | null | undefined,
     className?: string
 };
 
 const ProfileImage = ({src, className=""}:ProfileImageProps) => {
   return (
     <div className={`relative h12 w-12 overflow-hidden rounded-full ${className}`}>
-        {src === null ? <VscAccount className="h-full w-full"/> : <Image src={src} className="h12 w-12 overflow-hidden rounded-full outline-none" alt="Profile Image" quality={100} width={70} height={70}/>}
+        {(src == null || src == undefined) ? <VscAccount className="h-full w-full"/> : <Image src={src} className="h12 w-12 overflow-hidden rounded-full outline-none" alt="Profile Image" quality={100} width={70} height={70}/>}
     </div>
   )
 }
