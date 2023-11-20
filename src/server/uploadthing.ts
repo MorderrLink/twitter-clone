13989d48059194f3487714cb7fde8ac5124347b9
MorderrLink <burmenskiy90@bk.ru>
2023-11-20 21:39:51 +0300

    file commit edited #5

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -11,9 +11,9 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req, res }) => {
+    .middleware( ({ req, res }) => {
       // This code runs on your server before upload
-      const user = await auth(req, res);
+      const user = auth(req, res);
  
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
@@ -21,7 +21,7 @@ export const ourFileRouter = {
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {
+    .onUploadComplete( ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
  
