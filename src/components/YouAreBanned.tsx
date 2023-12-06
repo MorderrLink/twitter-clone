6049cb050405f08b38e969834704371ff8462f6f
MorderrLink <burmenskiy90@bk.ru>
2023-12-06 20:15:49 +0300

    Ban feature & google font addition addition( quotation marks fix )

--- src/components/YouAreBanned.tsx
+++ src/components/YouAreBanned.tsx
@@ -1,12 +1,10 @@
 
 
-function YouAreBunned() {
+export default function YouAreBunned() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-2">
         <h1 className="font-mono text-5xl">You are banned on Twitter Clone</h1>
-        <p className="font-light text-gray-500 text-xl">That means now you're not allowed to use any of it's features on this account</p>
+        <p className="font-light text-gray-500 text-xl">That means now you&apos;re not allowed to use any of it&apos;s features on this account</p>
     </div>
   )
 }
-
-export default YouAreBunned
